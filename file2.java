147e9abc69e45193438b61b960951ec411f67236
Ashish <ashishgupta6667@gmail.com>
2022-12-27 15:25:17 +0530

    all dome

--- file2.java
+++ file2.java
@@ -1,1 +1,5 @@
-hello ashish
+class file2{
+    public static void main(String[] args){
+        System.out.println("Hello ashish")
+    }
+}
--- file3.java
+++ file3.java
@@ -1,1 +1,7 @@
-hello
+public class file3{
+    public static void main(String[] args){
+        int a=5;
+        int b=4;
+        Sysytem.out.println(a, b);
+    }
+}
